70d8057e2e5a2ccd377554326110104bf43a6e82
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2021-10-15 11:08:55 +0300

    +rx java kullanılarak veriler çekildi +composite disposable kullanmak için rx javaya ihtiyaç duyulduğundan rx java kullanıldı +compositeDisposable

--- app/src/main/java/com/begers/kriptoretrofit/service/CryptoAPI.java
+++ app/src/main/java/com/begers/kriptoretrofit/service/CryptoAPI.java
@@ -4,11 +4,17 @@ import com.begers.kriptoretrofit.model.CryptoModel;
 
 import java.util.List;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface CryptoAPI {
 
     @GET("prices?key=eaaf35b443ed77522c3c114d37a6744c889827eb")
+    Observable<List<CryptoModel>> getData();
+
+    /*Call
+    @GET("prices?key=eaaf35b443ed77522c3c114d37a6744c889827eb")
     Call<List<CryptoModel>> getData();
+     */
 }
--- app/src/main/java/com/begers/kriptoretrofit/view/MainActivity.java
+++ app/src/main/java/com/begers/kriptoretrofit/view/MainActivity.java
@@ -13,12 +13,17 @@ import com.begers.kriptoretrofit.service.CryptoAPI;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     Retrofit retrofit;
     RecyclerView recyclerView;
     RecyclerViewAdapter recyclerViewAdapter;
+
+    CompositeDisposable compositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +49,16 @@ public class MainActivity extends AppCompatActivity {
 
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())  //rx java kullanacağımızı bildirmiş oluyoruz
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
-        loadDataApi();
+        loadDataApiObservable();
     }
 
-    private void loadDataApi(){
-        CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
+    /*
+    private void loadDataApiCall(){ //Call kullanılarak
+        final CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
 
         Call<List<CryptoModel>> call = cryptoAPI.getData();
 
@@ -71,5 +80,32 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+    }
+    */
+
+    private void loadDataApiObservable() { //Observable kullanılarak
+        final CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
+
+        compositeDisposable = new CompositeDisposable();
+
+        compositeDisposable.add(cryptoAPI.getData()
+        .subscribeOn(Schedulers.io()) //hangi thredde kayot olma işlemi yapılacak
+        .observeOn(AndroidSchedulers.mainThread()) //hangi thredde gözlemlenecek
+        .subscribe(this::handleResponse) //ortaya çıkan ürünü nerede ele alaçağız
+        );
+    }
+
+    private void handleResponse(List<CryptoModel> cryptoModelList){ //elealma işlemi burada yapılır.
+        cryptoModels = new ArrayList<>(cryptoModelList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        recyclerViewAdapter = new RecyclerViewAdapter(cryptoModels);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 }
